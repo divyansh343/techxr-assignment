3b57c2e4d8775f90ecb615ba662f9a3dbbbf100e
divyansh pal <70535666+divyansh343@users.noreply.github.com>
2022-02-26 12:30:39 +0530

    commit

--- src/components/sections/Videos-section/video-section.jsx
+++ src/components/sections/Videos-section/video-section.jsx
@@ -4,15 +4,15 @@ import './video-section.css'
 
 const VideoSection = ({ videos }) => {
   return (
-    <div className='container video-container pb-4'>
+    <div className='container mt-4 pt-4 pb-4'>
 
       <div className="headline mb-4 ">
         COURSE STRUCTURE
       </div>
 
-      <div className='video-layout'>
+      <div className='video-layout mb-4'>
         {videos
-          .filter((video, idx) => idx < 3)
+          .filter((video, idx) => idx < 31)
           .map(({ id, ...otherProps }) => (
             <Video key={id} {...otherProps} />
           ))}
